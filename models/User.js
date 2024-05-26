1bb2ecdaf90f58bb61c216588ab2e6d54749c191
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-05-27 01:00:27 +0530

    edit user success.

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email : {type : String, unique: true},
-    password: { type: String},
-    username: {type:String, required:false},
-    googleId: { type: String, unique: true, sparse: true }, // Unique Google ID
-    googleDisplayName: { type: String }, // Google display name
-    googleEmail: { type: String }, // Google email
+  email: { type: String, unique: true },
+  password: { type: String },
+  name: { type: String },
+  bio: { type: String },
+  phone: { type: String },
+  photo: { type: String },
+  isPrivate: { type: Boolean, default: false },
+  googleId: { type: String, unique: true, sparse: true }, // Unique Google ID
+  googleDisplayName: { type: String }, // Google display name
+  googleEmail: { type: String }, // Google email
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const authenticateUser = require("../middleware/authMiddleware");
+const User = require("../models/User");
+const multer = require("multer");
+const bcrypt = require("bcryptjs");
 
-router.get('/',(req,res) =>{
-  res.render('userProfile')
-})
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, res, cb) => {
+    cb(null, Date.now() + Path2D.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+router.get("/", authenticateUser, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    console.log("User:", user);
+    res.render("userProfile", { user });
+  } catch (error) {
+    res.status(500).send("Error Loading profile.");
+  }
+});
+
+router.post("/", authenticateUser, upload.single("photo"), async (req, res) => {
+  try {
+    const { name, bio, phone, email, password, isPrivate } = req.body;
+    console.log(req.body);
+    const user = await User.findById(req.userId);
+
+    if (req.file) {
+      user.photo = "/uploads" + req.file.filename;
+    }
+    user.name = name;
+    user.bio = bio;
+    user.phone = phone;
+    user.email = email;
+    user.isPrivate = isPrivate === "on";
+
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
+    }
+    await user.save();
+    res.redirect("/profile");
+  } catch (error) {
+    res.status(500).send("Error updating profile");
+  }
+});
 
 module.exports = router;
